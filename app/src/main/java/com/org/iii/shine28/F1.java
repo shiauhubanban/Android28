b7e943ac5c08e378f081e21cb32d2a2315fe4813
user <a>
2016-12-01 14:04:40 +0800

    v2

---
+++ app/src/main/java/com/org/iii/shine28/F1.java
@@ -0,0 +1,29 @@
+package com.org.iii.shine28;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class F1 extends Fragment {
+    public static F1 newInstance() {
+        F1 fragment = new F1();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_f1, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/org/iii/shine28/F2.java
@@ -0,0 +1,24 @@
+package com.org.iii.shine28;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class F2 extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+}
