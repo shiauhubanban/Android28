f9b061b1137a9ccc6eabcad028651daa3c5e6cd7
user <a>
2016-12-01 15:02:05 +0800

    v6 動態

--- app/src/main/java/com/org/iii/shine28/F1.java
+++ app/src/main/java/com/org/iii/shine28/F1.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class F1 extends Fragment {
@@ -24,7 +26,17 @@ public class F1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_f1, container, false);
+        View btn = view.findViewById(R.id.f1btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doBtn();
+            }
+        });
         return view;
+    }
+    private void doBtn(){
+        Log.v("brad", "F1 Button");
     }
 
 }
